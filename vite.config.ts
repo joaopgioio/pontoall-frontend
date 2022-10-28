889091c1076ddf960ebe0583489b61e1f1cc748e
Joao Paulo <joaopgioio@gmail.com>
2022-10-27 23:01:45 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-  //plugins: [vue()]
+  //plugins: [react()]
+  plugins: [vue()]
 })
