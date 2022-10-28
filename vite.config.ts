0e6f59429ecb7c3439c27bf9ef548a86f95863f9
Joao Paulo <joaopgioio@gmail.com>
2022-10-27 23:28:13 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from "node:url"
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vue from '@vitejs/plugin-vue'
@@ -5,5 +7,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   //plugins: [react()]
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
